875c8b2a15abb72a6b42d779f0f3978f21206101
Ritorta <meddesu@yandex.ru>
2022-12-18 07:16:40 +0300

    Не навижу двоящиеся запятые

--- Work №1/Task_004/Program.cs
+++ Work №1/Task_004/Program.cs
@@ -58,26 +58,51 @@ while (current <= numb)
    current = current + 1;
 }
 
-*/
 
-
-// Чётвертый вариант:
+// Чётвертый вариант: (Здесь я пытался вписать точку с оператором for)
 
 Console.Write("Enter the Number: ");
 int numb = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Чётные числа из диапазона: ");
 
+for (int count = 2; count <= numb; count++)
+
+    if (count %2 == 0)
+    {
+      Console.Write(count);
+         if (count <= numb - 1 ) 
+         // Я потратил кучу времени, чтобы написать это с точкой, прям целый день,
+         // самое главное, я додумался до этого НО! только через гугл я смог понять, что нехватало "-1", хех...
+         {
+            Console.Write(", ");
+         }
+       
+    }
+    Console.Write(".");
+        
+// Пятый вариант: (Здесь я пытался вписать точку с оператором while)
+
+*/
+
+Console.Write("Enter the Number: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
 int current = 2;
+
+Console.Write("Чётные числа из диапазона: ");
 
 while (current <= numb)
 {
    if (current %2 == 0)
-    Console.Write(current + ", ");
-    Console.Write("."); 
-    current = current + 1;
+      Console.Write(current);
+         if (current <= numb)
+      {
+         Console.Write(", ");
+      }
+      
+   current = current + 1;
 }
-        
- 
 
+Console.Write(".");
 
