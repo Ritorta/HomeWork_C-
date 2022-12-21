19996858b2a0d7efb065b61cd16238ecd733855a
Ritorta <meddesu@yandex.ru>
2022-12-21 20:13:51 +0300

    Task 2 Done

--- Work №2/Task_002/Program.cs
+++ Work №2/Task_002/Program.cs
@@ -8,8 +8,36 @@ Task №1:
 
 */
 
+int NumFree(int numb1)
+{
+    int Free = numb1 % 10;
+    int resoult = Free;
+    return resoult;
+    
+}
+
+Console.Write("Enter the number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
 
 
+    if(numb1 <= 99)
+    {
+        numb1 = numb1 = -1;
+        Console.Write("There is no third numb: ");
+        
+    }
+    else if (numb1 <= 999)
+    {
+        numb1 = numb1 % 10;
+    }
+    else if (numb1 <= 9999)
+    {
+        numb1 = numb1 / 10;
+    }
+    else if (numb1 <= 99999)
+    {
+        numb1 = numb1 / 100;
+    }
 
-
+Console.Write(NumFree(numb1));
     
