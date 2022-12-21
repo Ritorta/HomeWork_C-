6f203471fc33dbfb903ff61c88d4cbcbfc3e95cd
Ritorta <Meddesu@yandex.ru>
2022-12-21 10:16:05 +0300

    Task 3 Done

--- Work №2/Task_003/Program.cs
+++ Work №2/Task_003/Program.cs
@@ -6,4 +6,24 @@ Task №1:
 7 -> да
 1 -> нет
 
+
 */
+
+bool Day(int numb1)
+{
+    if(numb1 >= 6 & numb1 <= 7) 
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+Console.Write("Input number day of week from 1 to 7: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Day(numb1));
+
