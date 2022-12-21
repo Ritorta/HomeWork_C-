c14f03c285b8aa1144596b62c67595db90b212e4
Ritorta <Meddesu@yandex.ru>
2022-12-21 09:07:22 +0300

    Task 1 Done

--- Work №2/Task_001/Program.cs
+++ Work №2/Task_001/Program.cs
@@ -6,4 +6,74 @@ Task №1:
 782 -> 8
 918 -> 1
 
+
+
+int TwoNum(int numb1)
+{
+    int Two = numb1 % 100 / 10;
+    int resolt = Two;
+    
+
+    if(numb1 >= 100 && numb1 <= 999)
+    {
+       return resolt;
+    }
+    else
+    {
+     
+     return -1; // Можно ли здесь задать текстовое сообщение на возврат?
+     
+    }    
+   
+    
+}
+
+
+Console.Write("Imput any theree-digit number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(TwoNum(numb1));
+
+
+
+int TwoNum(int numb1)
+{
+    int Two = numb1 % 100 / 10;
+        
+    return Two;
+}
+
+
+Console.Write("Imput any theree-digit number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+if(numb1 >= 100 && numb1 <= 999)
+{
+    Console.Write(TwoNum(numb1));
+}
+else
+{
+    Console.Write("Invalid command, please enter the three-digit number.");
+}
+
 */
+
+int TwoNum(int numb1)
+{
+    int Two = numb1 % 100 / 10;
+    int resolt = Two;    
+    return resolt;
+}
+
+
+Console.Write("Imput any theree-digit number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+
+if(numb1 >= -999 & numb1 <= -100 || numb1 >= 100 & numb1 <= 999)
+{
+    Console.Write(TwoNum(numb1));
+}
+else
+{
+    Console.Write("Invalid command, please enter the three-digit number.");
+}
