63ac75e0d8157e76446a132ed141d71411e73322
Ritorta <meddesu@yandex.ru>
2023-01-05 19:36:58 +0300

    Question to Task_2

--- Work №2/Task_002/Program.cs
+++ Work №2/Task_002/Program.cs
@@ -7,50 +7,54 @@ Task №2:
 32679 -> 6
 
 
-// Вариант №1
-// Простой, рабочий, броский.
+// ---Вариант №1---
+// Простой, рабочий, броский и исправленный.
+
 int NumFree(int numb1)
 {
-    int Free = numb1 % 10;
-    int resoult = Free;
-    return resoult;
+
+int result = numb1;
     
+    if(numb1 <= 99)
+    {
+        numb1 = numb1 = -1;
+        result = numb1;
+        Console.Write("There is no third numb: error ");
+    }
+    else if (numb1 <= 999)
+    {
+        numb1 = numb1 % 10;
+        result = numb1;
+    }
+    else if (numb1 <= 9999)
+    {
+        numb1 = numb1 / 10 % 10;
+        result = numb1;
+    }
+    else if (numb1 <= 99999)
+    {
+        numb1 = numb1 / 100 % 10;
+        result = numb1;
+    }
+    
+return result;
 }
 
 Console.Write("Enter the number: ");
 int numb1 = Convert.ToInt32(Console.ReadLine());
 
-
-    if(numb1 <= 99)
-    {
-        numb1 = numb1 = -1;
-        Console.Write("There is no third numb: error ");
-        
-    }
-    else if (numb1 <= 999)
-    {
-        numb1 = numb1 % 10;
-    }
-    else if (numb1 <= 9999)
-    {
-        numb1 = numb1 / 10;
-    }
-    else if (numb1 <= 99999)
-    {
-        numb1 = numb1 / 100;
-    }
-
 Console.Write(NumFree(numb1));
-*/
-// Вариант №2
-// Через оператор "while" хотя особой разницы с Вариант№1 нету, зачем козе баян...?! 
+
+
+// ---Вариант №2---
+// Через оператор "while" хотя особой разницы с Вариант№1 нету. 
 
 Console.Write("Enter the number: ");
 int numb1 = Convert.ToInt32(Console.ReadLine());
 
 int NumFree(int numb1)
 {
-    int result =0;
+int result = 0;
 
     while(numb1 <= 99999)
     {
@@ -86,24 +90,67 @@ return result;
 }
 
 Console.Write(NumFree(numb1));
+*/
 
-/*
-// Вариант №3
-// Я так до конца и не понел как оно должно работать...
+// ---Вариант №3---
+// Теперь он может брать отрицальные значения и также ввёл ограничения на допустимый ввод.
+
+int NumFree(int numb1)
+{
+
+int result = numb1;  
+
+    if(numb1 <= 99 & numb1 >= -99)
+    {
+        Console.Write("There is no third numb: error ");
+        return -1;
+    }
+    else if (numb1 <= 999 & numb1 >= -999)
+    {
+       numb1 = numb1 % 10;
+       result = numb1;   
+    }
+      else if (numb1 <= 9999 & numb1 >= -9999)
+    {
+        numb1 = numb1 / 10 % 10;
+        result = numb1;
+    }
+    else if (numb1 <= 99999 & numb1 >= -99999)
+    {
+        numb1 = numb1 / 100 % 10;
+        result = numb1;
+    }
+    else
+    {
+        Console.Write("Please imput number -99999 to 99999: error ");
+        return -1;
+    }
+     
+return result;
+}
 
 Console.Write("Enter the number: ");
 int numb1 = Convert.ToInt32(Console.ReadLine());
-int leng = numb1.ToString().Length;
+
+Console.Write(NumFree(numb1));
+
+/*
+// Вариант который нагуглил и попытался в нём разобраться как он работает.
+// Я так до конца не понял, как оно должно работать...
+
+Console.Write("Enter the number: ");
+int numb1 = Convert.ToInt32(Console.ReadLine());
+int leng = numb1.ToString().Length; // Это рабоатет как абсолютно рандомная длинна?
 
 int NumFree(int numb1, int leng)
 {
     int result = 0;
-    int c = 1; // это строчка не рабобрана!!!
+    int c = 1; // это строчка не рабобрана(не понятна)!!!
 
-    for(int count = leng; count > 3; count--)
+    for(int count = leng; count > 3; count--) // Не совсем понял почему уменьшаем.
     {
         
-        c = c * 10; // это строчка не рабобрана!!!
+        c = c * 10; // это строчка не рабобрана(не понятна)!!!
                
     }
     result = (numb1 / c) % 10; 
@@ -114,9 +161,7 @@ int NumFree(int numb1, int leng)
         result = numb1;
         Console.Write("There is no third numb: error ");
     }
-
-
-return result;
+    return result;
 }
 
 Console.Write(NumFree(numb1, leng));
