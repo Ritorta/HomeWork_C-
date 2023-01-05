dd549b7c5de0f19b9552accb690cd92c9085feb6
Ritorta <meddesu@yandex.ru>
2023-01-06 02:39:08 +0300

    save_2

--- Work №3/Task_001/Program.cs
+++ Work №3/Task_001/Program.cs
@@ -7,6 +7,46 @@ Task №1:
 23432 -> да
 
 
-
-// Вариант №1
+/*
+// ---Вариант №1---
+// Пробуемс реализовать
 */
+
+Console.Write("Imput number: "); 
+int numb = Convert.ToInt32(Console.ReadLine());
+
+
+
+int Reverse (int numb)
+{
+
+int temp = 0;
+
+    while (numb > 0)
+    {
+        temp = temp * 10 + numb % 10;
+        numb = numb / 10;
+    }
+return temp;
+}
+
+int Cheek (int n)
+{
+    int rev_n = Reverse(n);
+
+     if(rev_n == n)
+        {
+            Console.WriteLine("Yes is poly");
+            return 1;
+        }
+        else
+        {
+            Console.WriteLine("Dont no poly");
+            return 0;
+        }
+}
+
+Console.WriteLine(Reverse(numb));
+Console.WriteLine(Cheek(numb));
+
+
