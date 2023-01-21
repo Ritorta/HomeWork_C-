c17e0f3fea2e96de71a028ce8703ae2b59988504
Ritorta <meddesu@yandex.ru>
2023-01-21 05:25:47 +0300

    Work 4. Done.

--- Work №4/Task_003/Program.cs
+++ Work №4/Task_003/Program.cs
@@ -6,23 +6,33 @@ Task №3:
 6, 1, 33 -> [6, 1, 33]
 
 */
-// ---Вариант №3---
+// ---Вариант №1---
 // 
 
-int[] ArrayInput(int )
+int[] ArrayInput(int leng)
 {
+int[] array = new int[leng];
 
-}
-
-
-
-int[] CreateArray(int size)
-{
-    int[] array = new int[size];
-    for(int i=0; i < size; i++)
+    for(int i =0; i < leng; i++)
     {
-        Console.Write($"Input a {i + 1} element: ");
+        Console.Write($"Enter the array number {i + 1}: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
-    return array;
+return array;    
 }
+
+void ArrayShow(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+Console.WriteLine();
+}
+
+Console.Write("Imput size array: ");
+int leng = Convert.ToInt32(Console.ReadLine());
+
+int[] result = ArrayInput(leng);
+ArrayShow(result);
+
