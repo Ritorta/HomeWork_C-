f41c7ea2601d60e7ef0d906ffd27b589b4c42757
Ritorta <meddesu@yandex.ru>
2023-01-22 19:18:38 +0300

    almost Done last task

--- Work №5/Task_003/Program.cs
+++ Work №5/Task_003/Program.cs
@@ -4,32 +4,24 @@ Task №3:
 
 [3 7 22 2 78] -> 76
 
-
+*/
 // ---Вариант №1---
 // 
 
-int[] IntRandomArray(int length, int minValue, int maxValue)
+double[] DoubleRandomArray(int length, int minValue, int maxValue)
 {
 int[] array = new int[length];
+double[] array2 = new double[length];
+double[] array3 = new double[length];
     for(int i = 0; i < length; i++)
     {
         array[i] = new Random().Next(minValue, maxValue + 1);
-        
+        array2[i] = new Random().NextDouble();
+        array2[i] = Math.Round(array2[i], 2);
+        array3[i] = array[i] + array2[i];
     }
-    return array;
+    return array3;
 }
-
-double[] DoubleRandomArray(int length)
-{
-double[] array = new double[length];
-    for(int i = 0; i < length; i++)
-    {
-        array[i] = new Random().NextDouble();
-        
-    }
-    return array;
-}
-
 
 void ShowArray(double[] array)
 {
@@ -40,28 +32,33 @@ void ShowArray(double[] array)
 Console.WriteLine();
 }
 
-int Difference(int[] array, int min, int max)
+int Difference(int[] array, int minValue, int maxValue)
 {
-    
     for(int i = 0; i < array.Length; i++)
     {
-        if(array[i] > min)
+    
+        if(array[i] < minValue)
         {
-            min = array[i];
+            minValue = array[i];
         }
-        else if (array[i] < max)
+        if (array[i] > maxValue)
         {
-            max = array[i];
+            maxValue = array[i];
         }
+        
     }
-return min - max;
+return maxValue - minValue;
 }
 
-Console.Write("/1 - ");
+Console.Write("Enter the size array: ");
 int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the minimum value length array: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the maximum value length array: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+double[] newArray = DoubleRandomArray(length, minValue, maxValue);
+ShowArray(newArray);
+//double Sum = Difference(newArray);
 
 
-
-double[] newArray2 = DoubleRandomArray(length);
-ShowArray(newArray2);
-*/
