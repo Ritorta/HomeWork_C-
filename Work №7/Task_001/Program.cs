7f088216447aadb80b3deb7c03a2d2c81fa43f33
Ritorta <Meddesu@yandex.ru>
2023-01-25 12:56:28 +0500

    W7T1 Done

--- Work №7/Task_001/Program.cs
+++ Work №7/Task_001/Program.cs
@@ -11,5 +11,47 @@ m = 3, n = 4.
 // ---Вариант №1---
 // 
 
+double[,] RandomDouble2D ()
+{
+Console.Write("Input row: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input coluns: ");
+int colouns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minValue: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input maxValue: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+    double[,] array = new double[rows, colouns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colouns; j++)
+        {
+            array[i,j] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+            array[i,j] = Math.Round(array[i,j], 3);
+        }
+    }
+return array;    
+}
+
+void ShowArray2D (double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 
+
+double[,] newArray = RandomDouble2D();
+ShowArray2D(newArray);
+
+
+// https://www.youtube.com/watch?v=g4nGMWwf7OQ
--- Work №7/Task_003/Program.cs
+++ Work №7/Task_003/Program.cs
@@ -11,3 +11,54 @@ Task №3:
 */
 // ---Вариант №1---
 // 
+
+int[,] RandomArray2D(int rows, int colouns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colouns];
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < colouns; j++)
+            {
+                array[i,j] = new Random().Next(minValue, maxValue + 1);
+            }
+        }
+return array;        
+}
+
+void ShowArray2D(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] middleSum (int[,] array)
+{
+    int Sum = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+
+        }
+    }
+return;
+} 
+
+Console.Write("Input row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input coluns: ");
+int colouns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minValue: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input maxValue: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = RandomArray2D(row, colouns, minValue, maxValue);
+ShowArray2D(newArray);
