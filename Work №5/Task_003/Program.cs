cda22d6950cab25bf2548595e14c1bfb02301d7b
Ritorta <meddesu@yandex.ru>
2023-01-25 21:19:10 +0300

    pph2

--- Work №5/Task_003/Program.cs
+++ Work №5/Task_003/Program.cs
@@ -4,9 +4,9 @@ Task №3:
 
 [3 7 22 2 78] -> 76
 
-*/
+
 // ---Вариант №1---
-// 
+// Рандом вещественных чисел я реализовал как склейку нескольких рандомов в ячейки памяти.
 
 double[] DoubleRandomArray(int length, int minValue, int maxValue)
 {
@@ -64,3 +64,61 @@ double Sum = Difference(newArray, maxValue, minValue);
 double Sum1 = Math.Round(Sum, 2);
 Console.Write($"Difference min or max: {Sum1}");
 
+*/
+
+// ---Вариант №2---
+// Это вариант с реализацией вещественных чисел в одну строку как и гговорил преподаватель, наконец у меня получилось сделать как он сказал.
+
+double[] DoubleRandomArray(int length, int minValue, int maxValue)
+{
+double[] array = new double[length];
+
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+        array[i] = Math.Round(array[i], 2);
+    }
+    return array;
+}
+
+
+void ShowArray(double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+}
+
+double Difference(double[] array, double minValue, double maxValue)
+{
+
+    for(int i = 0; i < array.Length; i++)
+    {
+    
+        if (array[i] > maxValue)
+        {
+            maxValue = array[i];
+        }
+        if(array[i] < minValue)
+        {
+            minValue = array[i];
+        }
+        
+    }
+return maxValue - minValue;
+}
+
+Console.Write("Enter the size array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the minimum value length array: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the maximum value length array: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+double[] newArray = DoubleRandomArray(length, minValue, maxValue);
+ShowArray(newArray);
+double Sum = Difference(newArray, maxValue, minValue);
+double Sum1 = Math.Round(Sum, 2);
+Console.Write($"Difference min or max: {Sum1}");
